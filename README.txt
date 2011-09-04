9394a8326a898d5237e01ca572db9c17e3ca6fcb
Gisle Aas <gisle@aas.no>
2011-09-04 21:56:16 +0200

    Add setup.py and some supporting files

---
+++ README.txt
@@ -0,0 +1,5 @@
+ISO Week
+========
+
+The isoweek module provide a class representing a week spanning Monday to Sunday.
+There are 52 or 53 weeks in a year.
---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = 'isoweek',
+    version = '0.1.0',
+    description = 'Objects representing a week',
+    author='Gisle Aas',
+    author_email='gisle@aas.no',
+    url='http://github.com/gisle/isoweek',
+    py_modules=['isoweek'],
+    license='LICENSE.txt',
+    long_description=open("README.txt").read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
