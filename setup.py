4848cc22350568b0a5e5a9c3bf4fe4f03331f888
Gisle Aas <gisle.aas@it.uib.no>
2017-01-04 15:40:55 +0100

    Increment version number to 1.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name = 'isoweek',
-    version = '1.3.1',
+    version = '1.3.2',
     description = 'Objects representing a week',
     author='Gisle Aas',
     author_email='gisle@aas.no',
